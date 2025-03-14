59904c788eff3762d77406a4dca344a4243a08d3
Vinit Nayak <peanutbutter@google.com>
2025-03-14 09:55:14 -0400

    Fix app pair crash
    * With the change made in ag/31615582, we assumed
    that top level + stage level split roots would always be
    included in the transition, that isn't the case when an app pair
    is already showing and then we switch to another app pair (stage
    roots aren't included since they aren't modified in that scenario)
    * Now shell sends the entire tree. Also modified SplitScreenUtils
    to consider TRANSIT_CHANGE changes as well for the scenario where
    one app of an app pair is already full screen and then we launch an
    app pair which has that same app, the stage root there undergoes a change
    and not a open/close transition.
    * One KI is that the animation doesn't fully work when launching a second
    app pair from taskbar when split is already showing (though it is an
    improvement from current behavior/animation). This is because when the second
    app pair launches, the tasks' snapshot windows/leashes are passed back as
    leafs, however these snapshots have not been parented to be under the stage roots.
    So both snapshots are positioned at 0, 0 causing overlap. Will fix that in
    a followup
    
    Bug: 399037701
    Test: Open app pairs from desktop windowing + normal taskbar
    Flag: EXEMPT bugfix
    
    Change-Id: I83adffd16fea98b8e877dfbe1ba1ef4a3d08fcb9

--- quickstep/src/com/android/quickstep/util/SplitScreenUtils.kt
+++ quickstep/src/com/android/quickstep/util/SplitScreenUtils.kt
@@ -17,14 +17,13 @@
 package com.android.quickstep.util
 
 import android.util.Log
-import android.view.WindowManager.TRANSIT_OPEN
-import android.view.WindowManager.TRANSIT_TO_FRONT
+import android.view.WindowManager.TRANSIT_CHANGE
 import android.window.TransitionInfo
 import android.window.TransitionInfo.Change
 import android.window.TransitionInfo.FLAG_FIRST_CUSTOM
 import com.android.launcher3.util.SplitConfigurationOptions
+import com.android.wm.shell.shared.TransitionUtil
 import com.android.wm.shell.shared.split.SplitBounds
-import java.lang.IllegalStateException
 
 class SplitScreenUtils {
     companion object {
@@ -54,14 +53,7 @@ class SplitScreenUtils {
         ): Pair<Change, List<Change>>? {
             val parentToChildren = mutableMapOf<Change, MutableList<Change>>()
             val hasParent = mutableSetOf<Change>()
-            // filter out anything that isn't opening and the divider
-            val taskChanges: List<Change> =
-                transitionInfo.changes
-                    .filter { change ->
-                        (change.mode == TRANSIT_OPEN || change.mode == TRANSIT_TO_FRONT) &&
-                            change.flags < FLAG_FIRST_CUSTOM
-                    }
-                    .toList()
+            val taskChanges: List<Change> = getNonClosingChanges(transitionInfo)
 
             // 1. Build Parent-Child Relationships
             for (change in taskChanges) {
@@ -89,6 +81,17 @@ class SplitScreenUtils {
                 Log.w(TAG, "No top parent found")
                 null
             }
+        }
+
+
+        /** @return includes only opening + [TRANSIT_CHANGE] changes and the divider */
+        private fun getNonClosingChanges(transitionInfo: TransitionInfo): List<Change> {
+            return transitionInfo.changes
+                .filter { change ->
+                    (TransitionUtil.isOpeningMode(change.mode) || change.mode == TRANSIT_CHANGE)
+                            && change.flags < FLAG_FIRST_CUSTOM
+                }
+                .toList()
         }
     }
 }

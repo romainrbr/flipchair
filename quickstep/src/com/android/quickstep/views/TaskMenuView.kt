3d348a0d277869268332d13e6db22543f23ae310
Jordan Silva <silvajordan@google.com>
2025-05-20 16:47:17 +0100

    Add focus border animation to IconAppChipView
    Fix: 366177160
    Flag: com.android.launcher3.enable_overview_icon_menu
    Test: Manual using Keyboard. Access the app chip via TAB and navigate through the task menu.
    Test: Manual using Mouse. Hover over the Chip and the Task Menu.
    Change-Id: I785ad8723e5255186d744a20992ac0cd0968b380

--- quickstep/src/com/android/quickstep/views/TaskMenuView.kt
+++ quickstep/src/com/android/quickstep/views/TaskMenuView.kt
@@ -97,7 +97,7 @@ constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int = 0) :
     }
 
     override fun handleClose(animate: Boolean) {
-        animateOpenOrClosed(true, animated = false)
+        animateOpenOrClosed(closing = true, animated = animate)
     }
 
     override fun isOfType(type: Int): Boolean = (type and TYPE_TASK_MENU) != 0
@@ -171,12 +171,6 @@ constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int = 0) :
                 ResourcesCompat.getDrawable(
                     resources,
                     R.drawable.app_chip_menu_item_bg,
-                    context.theme,
-                )
-            menuOptionView.foreground =
-                ResourcesCompat.getDrawable(
-                    resources,
-                    R.drawable.app_chip_menu_item_fg,
                     context.theme,
                 )
         }
@@ -295,7 +289,7 @@ constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int = 0) :
     private fun animateOpen() {
         menuTranslationYBeforeOpen = translationY
         menuTranslationXBeforeOpen = translationX
-        animateOpenOrClosed(false)
+        animateOpenOrClosed(closing = false)
         mIsOpen = true
     }
 

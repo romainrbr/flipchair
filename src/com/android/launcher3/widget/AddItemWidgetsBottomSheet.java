1940ab1401563ebd2b44d309d24e33652606a534
Pun Butrach <pun.butrach@gmail.com>
2025-11-07 17:45:59 +0700

    fix: Correctly import windowInsets & Set submodule to Lawnchair

--- src/com/android/launcher3/widget/AddItemWidgetsBottomSheet.java
+++ src/com/android/launcher3/widget/AddItemWidgetsBottomSheet.java
@@ -26,10 +26,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.view.WindowInsets;
+import android.view.WindowInsets.Type;
 import android.widget.ScrollView;
 
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.dragndrop.AddItemActivity;
 import com.android.launcher3.views.AbstractSlideInView;
 

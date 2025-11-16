e1b2f18779e1317b5b09597ac500c2364110bc17
Pun Butrach <pun.butrach@gmail.com>
2025-11-16 18:02:20 +0700

    feat: SystemUI Plugin Android 16 QPR1

--- systemUI/plugin/src/com/android/systemui/plugins/clocks/ClockProviderPlugin.kt
+++ systemUI/plugin/src/com/android/systemui/plugins/clocks/ClockProviderPlugin.kt
@@ -13,110 +13,62 @@
  */
 package com.android.systemui.plugins.clocks
 
-import com.android.internal.annotations.Keep
-import org.json.JSONObject
+import android.content.Context
+import com.android.systemui.plugins.Plugin
+import com.android.systemui.plugins.annotations.GeneratedImport
+import com.android.systemui.plugins.annotations.ProtectedInterface
+import com.android.systemui.plugins.annotations.ProtectedReturn
+import com.android.systemui.plugins.annotations.ProvidesInterface
+
+/** A Plugin which exposes the ClockProvider interface */
+@ProtectedInterface
+@ProvidesInterface(action = ClockProviderPlugin.ACTION, version = ClockProviderPlugin.VERSION)
+interface ClockProviderPlugin : Plugin, ClockProvider {
+    companion object {
+        const val ACTION = "com.android.systemui.action.PLUGIN_CLOCK_PROVIDER"
+        const val VERSION = 1
+    }
+}
+
+/** Interface for building clocks and providing information about those clocks */
+@ProtectedInterface
+@GeneratedImport("java.util.List")
+@GeneratedImport("java.util.ArrayList")
+interface ClockProvider {
+    /** Initializes the clock provider with debug log buffers */
+    fun initialize(buffers: ClockMessageBuffers?)
+
+    @ProtectedReturn("return new ArrayList<ClockMetadata>();")
+    /** Returns metadata for all clocks this provider knows about */
+    fun getClocks(): List<ClockMetadata>
+
+    @ProtectedReturn("return null;")
+    /** Initializes and returns the target clock design */
+    fun createClock(ctx: Context, settings: ClockSettings): ClockController?
+
+    @ProtectedReturn("return new ClockPickerConfig(\"\", \"\", \"\", null);")
+    /** Settings configuration parameters for the clock */
+    fun getClockPickerConfig(settings: ClockSettings): ClockPickerConfig
+}
 
 /** Identifies a clock design */
 typealias ClockId = String
 
-data class AodClockBurnInModel(
-    val scale: Float,
-    val translationX: Float,
-    val translationY: Float,
-)
-
-/** Tick rates for clocks */
-enum class ClockTickRate(val value: Int) {
-    PER_MINUTE(2), // Update the clock once per minute.
-    PER_SECOND(1), // Update the clock once per second.
-    PER_FRAME(0), // Update the clock every second.
-}
-
-/** Some data about a clock design */
+/** Some metadata about a clock design */
 data class ClockMetadata(
+    /** Id for the clock design. */
     val clockId: ClockId,
-)
-
-/** Render configuration for the full clock. Modifies the way systemUI behaves with this clock. */
-data class ClockConfig(
-    val id: String,
-
-    /** Localized name of the clock */
-    val name: String,
-
-    /** Localized accessibility description for the clock */
-    val description: String,
-
-    /** Transition to AOD should move smartspace like large clock instead of small clock */
-    val useAlternateSmartspaceAODTransition: Boolean = false,
-
-    /** True if the clock will react to tone changes in the seed color. */
-    val isReactiveToTone: Boolean = true,
-
-    /** True if the clock is large frame clock, which will use weather in compose. */
-    val useCustomClockScene: Boolean = false,
-)
-
-/** Render configuration options for a clock face. Modifies the way SystemUI behaves. */
-data class ClockFaceConfig(
-    /** Expected interval between calls to onTimeTick. Can always reduce to PER_MINUTE in AOD. */
-    val tickRate: ClockTickRate = ClockTickRate.PER_MINUTE,
-
-    /** Call to check whether the clock consumes weather data */
-    val hasCustomWeatherDataDisplay: Boolean = false,
 
     /**
-     * Whether this clock has a custom position update animation. If true, the keyguard will call
-     * `onPositionUpdated` to notify the clock of a position update animation. If false, a default
-     * animation will be used (e.g. a simple translation).
+     * true if this clock is deprecated and should not be used. The ID may still show up in certain
+     * locations to help migrations, but it will not be selectable by new users.
      */
-    val hasCustomPositionUpdatedAnimation: Boolean = false,
+    val isDeprecated: Boolean = false,
 
-    /** True if the clock is large frame clock, which will use weatherBlueprint in compose. */
-    val useCustomClockScene: Boolean = false,
+    /**
+     * Optional mapping of a legacy clock to a new id. This will map users that already are using
+     * `clockId` to the `replacementTarget` instead. The provider should still support the old id
+     * w/o crashing, but can consider it deprecated and the id reserved.
+     */
+    val replacementTarget: ClockId? = null,
 )
-
-/** Structure for keeping clock-specific settings */
-@Keep
-data class ClockSettings(
-    val clockId: ClockId? = null,
-    val seedColor: Int? = null,
-) {
-    // Exclude metadata from equality checks
-    var metadata: JSONObject = JSONObject()
-
-    companion object {
-        private val KEY_CLOCK_ID = "clockId"
-        private val KEY_SEED_COLOR = "seedColor"
-        private val KEY_METADATA = "metadata"
-
-        fun serialize(setting: ClockSettings?): String {
-            if (setting == null) {
-                return ""
-            }
-
-            return JSONObject()
-                .put(KEY_CLOCK_ID, setting.clockId)
-                .put(KEY_SEED_COLOR, setting.seedColor)
-                .put(KEY_METADATA, setting.metadata)
-                .toString()
-        }
-
-        fun deserialize(jsonStr: String?): ClockSettings? {
-            if (jsonStr.isNullOrEmpty()) {
-                return null
-            }
-
-            val json = JSONObject(jsonStr)
-            val result =
-                ClockSettings(
-                    if (!json.isNull(KEY_CLOCK_ID)) json.getString(KEY_CLOCK_ID) else null,
-                    if (!json.isNull(KEY_SEED_COLOR)) json.getInt(KEY_SEED_COLOR) else null
-                )
-            if (!json.isNull(KEY_METADATA)) {
-                result.metadata = json.getJSONObject(KEY_METADATA)
-            }
-            return result
-        }
-    }
-}

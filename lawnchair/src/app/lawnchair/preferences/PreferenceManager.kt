6a514235f93cee8ddb85cb0a3b35947fb6f66270
Pun Butrach <pun.butrach@gmail.com>
2025-12-02 00:01:37 +0700

    feat: Enable bulk app loading by default

--- lawnchair/src/app/lawnchair/preferences/PreferenceManager.kt
+++ lawnchair/src/app/lawnchair/preferences/PreferenceManager.kt
@@ -105,7 +105,7 @@ class PreferenceManager @Inject constructor(
     val searchResultSettingsEntry = BoolPref("pref_searchResultSettingsEntry", false, recreate)
     val searchResulRecentSuggestion = BoolPref("pref_searchResultRecentSuggestion", false, recreate)
 
-    val allAppBulkIconLoading = BoolPref("pref_allapps_bulk_icon_loading", false, recreate)
+    val allAppBulkIconLoading = BoolPref("pref_allapps_bulk_icon_loading", true, recreate)
 
     val themedIcons = BoolPref("themed_icons", false, recreate)
     val drawerThemedIcons = BoolPref("drawer_themed_icons", false, recreate)
